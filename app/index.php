a2f65f20dcd4172ec585aa911af2a655060a5f9a
aimalessio <113421737+aimalessio@users.noreply.github.com>
2024-10-22 18:12:37 +0200

    docker shit

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "mysql"; // Le host est le nom du service, présent dans le docker-compose.yml
+$dbname = "my-wonderful-website";
+$charset = "utf8";
+$port = "3306";
+?>
+
+<html>
+<head>
+    <title>Drenthe College docker web server</title>
+</head>
+<body>
+<h2>Hello world!</h2>
+</body>
+</html>
